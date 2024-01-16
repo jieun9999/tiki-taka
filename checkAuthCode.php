ae6c1abc8ee69d6d53e9f8f1dc97a5e44343b229
jieun <kindjjee@gmail.com>
2024-01-16 15:46:41 +0000

    이메일 인증완료

---
+++ checkAuthCode.php
@@ -0,0 +1,38 @@
+<?php
+
+//include는 파일이 없는 경우에도 스크립트 실행을 계속하고, require는 파일이 없는 경우 스크립트 실행을 중단합니다.
+require 'db_connect.php'; // 데이터베이스 연결
+
+$email = $_POST['email']; // 유저로부터 받은 이메일
+$authCode = $_POST['authCode'];// 유저로부터 받은 인증번호
+
+// 현재 시간
+$currentDatetime = date('Y-m-d H:i:s');
+
+// 1. 인증번호 일치 여부 및 인증코드 날짜와 현재 시간 비교
+//이메일과 인증번호가 모두 일치하고, 인증번호 생성 시간이 현재 시간으로부터 10분 이내인 경우에만 인증 성공으로 간주
+$sql = "SELECT * FROM userAuth WHERE email = :email
+                                AND auth_code = :authCode";
+
+$stmt = $conn->prepare($sql);
+$stmt->bindParam(':email', $email, PDO::PARAM_STR);
+$stmt->bindParam(':authCode', $authCode, PDO::PARAM_INT);
+$stmt->execute();
+
+$result = $stmt->fetch(PDO::FETCH_ASSOC);
+
+if ($result) {
+    $authCodeDate = $result['auth_code_date'];
+    // 인증 코드 생성 시간과 현재 시간을 비교하여 10분 이내에 유효한지 확인
+    $diff = strtotime($currentDatetime) - strtotime($authCodeDate);
+
+   if ($diff <= 600) {
+        echo json_encode(["success" => true, "message" => "이메일 인증에 성공하였습니다"]);
+    } else {
+        echo json_encode(["success" => false, "message" => "인증번호가 만료되었습니다. 재발급 받으세요"]);
+    }
+} else {
+    echo json_encode(["success" => false, "message" => "이메일 인증에 실패하였습니다"]);
+}
+
+?>
--- sendVerificationCode.php
+++ sendVerificationCode.php
@@ -9,19 +9,60 @@ $email = $_POST['email'];// 클라이언트로부터 받은 이메일
 $verificationCode = rand(100000, 999999); // 6자리 랜덤 숫자 생성
 
 //2. db에 이메일, 인증번호, 생성날짜를 저장함
-$sql = "INSERT INTO userAuth (email, verification_code, created_at) VALUES (::email, :code, NOW())";
+$sql = "INSERT INTO userAuth (email, auth_code, auth_code_date) VALUES (:email, :code, NOW())";
 $stmt = $conn-> prepare($sql);
 $stmt->bindParam(':email', $email, PDO::PARAM_STR);
 $stmt->bindParam(':code', $verificationCode, PDO::PARAM_INT);
-$stmt->execute();
-$result = $stmt->fetchAll();
+$insertResult = $stmt->execute();
 
 // 3. 이메일 발송
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
+
+//실제 PHPMailer 라이브러리가 설치된 경로로 수정
+require 'vendor/phpmailer/phpmailer/src/Exception.php';
+require 'vendor/phpmailer/phpmailer/src/PHPMailer.php';
+require 'vendor/phpmailer/phpmailer/src/SMTP.php';
+
+$mail = new PHPMailer(true);
+
+// "Simple Mail Transfer Protocol"의 약자로, 인터넷을 통한 이메일 전송에 사용되는 프로토콜
+try {
+    // 서버 설정
+    $mail->isSMTP();                                      // SMTP를 사용
+    $mail->Host = 'smtp.gmail.com';                       // 메일 서버 지정
+    $mail->SMTPAuth = true;                               // SMTP 인증 사용
+    $mail->Username = 'teamjidong@gmail.com';             // Gmail 주소
+    $mail->Password = 'gzto jmsd bzqp pmrv';              // Gmail 앱비밀번호
+    $mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;   // TLS 암호화 사용
+    $mail->Port = 587;                                    // TCP 포트 연결
+
+    // 수신자 설정
+    $mail->setFrom('teamjidong@gmail.com', 'Mailer');     // 보내는 사람
+    $mail->addAddress($email);     // 받는 사람
+
+    // 컨텐츠 설정
+    $mail->isHTML(true);                     // HTML 이메일 설정
+    $mail->CharSet = 'UTF-8';                //한글 인코딩
+    $mail->Subject = '이메일 인증 코드 발송';    // 이메일 제목
+    $mail->Body = "<h1>이메일 인증 코드</h1>
+               <p>귀하의 인증 코드: <strong>$verificationCode</strong></p>
+               <p><strong>주의: 이 인증번호는 10분 내에 만료됩니다.</strong></p>";
+               //html 메세지 본문
 
 
+    // 이메일 전송
+    $mail->send();
+    $emailSent = true;
+
+}catch (Exception $e) {
+    $emailSent = false;
+    }
 
 
-if (count($result) > 0) {
+// 최종결과 반환
+// 서버의 응답을 `Boolean` 값으로 간단하게 처리
+if ($insertResult && $emailSent) {
     echo json_encode(true); // 인증번호 전송 성공
 } else {
     echo json_encode(false); // 인증번호 전송 실패
