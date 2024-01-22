a990a6c0e352bc20a996f751e0ed88d67af9fe0c
jieun <kindjjee@gmail.com>
2024-01-22 16:46:41 +0000

    임시 비밀번호 전송 완료

--- Auth/sendTemporaryPassword.php
+++ Auth/sendTemporaryPassword.php
@@ -8,17 +8,36 @@ require '../db_connect.php'; // 데이터베이스 연결
 
 $email = $_POST['email'];// 클라이언트로부터 받은 이메일
 
-//1. 인증번호 생성
-$verificationCode = rand(100000, 999999); // 6자리 랜덤 숫자 생성
+//1. 임시 비밀번호 생성
+function generateTemporaryPassword($length = 10) {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@#$%^&*()';
+    $charactersLength = strlen($characters);
+    $randomPassword = '';
+    $codeDate = date("Y-m-d H:i:s"); // 임시 비밀번호 생성시간
 
-//2. db에 이메일, 인증번호, 생성날짜를 저장함
-$sql = "INSERT INTO userAuth (email, auth_code, auth_code_date) VALUES (:email, :code, NOW())";
+    for ($i = 0; $i < $length; $i++) {
+        $randomPassword .= $characters[rand(0, $charactersLength - 1)];
+    }
+
+    
+    return array('password' => $randomPassword, 'codeDate' => $codeDate);
+}
+
+//2. 임시비밀번호 및 생성날짜 업데이트
+$temporaryPasswordData = generateTemporaryPassword(); // 10자리 임시 비밀번호 및 생성 날짜
+$temporaryPassword = $temporaryPasswordData['password'];
+$codeDate = $temporaryPasswordData['codeDate'];
+
+$sql = "UPDATE userAuth SET temporary_password = :temporary_password, 
+                            temporary_password_date = :codeDate
+                            WHERE email = :email";
 $stmt = $conn-> prepare($sql);
-$stmt->bindParam(':email', $email, PDO::PARAM_STR);
-$stmt->bindParam(':code', $verificationCode, PDO::PARAM_INT);
-$insertResult = $stmt->execute();
+$stmt->bindParam(':email', $email);
+$stmt->bindParam(':temporary_password', $temporaryPassword);
+$stmt->bindParam(':codeDate', $codeDate);
+$updateResult = $stmt->execute();
 
-// 3. 이메일 발송
+//3. 이메일 발송
 use PHPMailer\PHPMailer\PHPMailer;
 use PHPMailer\PHPMailer\Exception;
 
@@ -49,10 +68,9 @@ try {
     $mail->CharSet = 'UTF-8';                //한글 인코딩
     $mail->Subject = '이메일 인증 코드 발송';    // 이메일 제목
     $mail->Body = "<h1>이메일 인증 코드</h1>
-               <p>귀하의 인증 코드: <strong>$verificationCode</strong></p>
+               <p>귀하의 임시비밀번호: <strong>$temporaryPassword</strong></p>
                <p><strong>주의: 이 인증번호는 10분 내에 만료됩니다.</strong></p>";
                //html 메세지 본문
-
 
     // 이메일 전송
     $mail->send();
@@ -64,11 +82,13 @@ try {
 
 
 // 최종결과 반환
-// 서버의 응답을 `Boolean` 값으로 간단하게 처리
-if ($insertResult && $emailSent) {
-    echo json_encode(true); // 인증번호 전송 성공
+if ($updateResult && $emailSent) {
+    echo json_encode(["success" => true, "message" => "이메일 주소로 전송된 임시 비밀번호를 확인하세요."]);
+
 } else {
-    echo json_encode(false); // 인증번호 전송 실패
+    echo json_encode(["success" => true, "message" => "이메일 전송에 실패하였습니다."]);
 }
+
+
 
 ?>
