4a968eee3ab9ce7cadbd0e6076ae70009d86c7bd
jieun <kindjjee@gmail.com>
2024-02-25 08:35:13 +0000

    삭제 시 post 요청으로 바꿈

--- Story/deleteComment.php
+++ Story/deleteComment.php
@@ -3,9 +3,14 @@ ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 require '../db_connect.php';
-$commentId = isset($_GET['commentId']) ? $_GET['commentId'] : null;
- error_log("story commentId: " . $commentId);
 
+// JSON 형식의 요청 본문 데이터 읽기
+$json = file_get_contents('php://input');
+// JSON 데이터를 PHP 배열로 변환
+$data = json_decode($json, true);
+
+// commentId 추출
+$commentId = isset($data['commentId']) ? $data['commentId'] : null;
 
 if($commentId !== null){
     try{
