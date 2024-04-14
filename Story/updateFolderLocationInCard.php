6bd9ce67e4ddd4eb390937cdb2e95417638a735e
jieun <kindjjee@gmail.com>
2024-04-14 06:34:56 +0000

    게시물 폴더 변경 시 알림 처리

--- Story/updateFolderLocationInCard.php
+++ Story/updateFolderLocationInCard.php
@@ -11,8 +11,9 @@ $data = json_decode($json, true);
 
 $folderId = isset($data['folder_id']) ? $data['folder_id'] : null;
 $cardId = isset($data['card_id']) ? $data['card_id'] : null;
+$partnerId = isset($data['partnerId']) ? $data['partnerId'] : null;
 
-if($folderId !== null && $cardId !== null){
+if($folderId !== null && $cardId !== null && $partnerId !== null){
     try{
         $sql = "UPDATE storyCard SET folder_id = :folder_id
                 WHERE card_id = :card_id";
@@ -24,22 +25,47 @@ if($folderId !== null && $cardId !== null){
         header('Content-Type: application/json'); // JSON 응답 헤더 추가
         if($success){
             if($stmt ->rowCount() > 0){
-                // 실제로 데이터가 업데이트 되었음
-                echo json_encode(['success' => true]);
-            }else{
-                // 데이터가 업데이트되지 않았음을 의미할 수 있음 (이미 같은 값이었을 경우 등)
-                echo json_encode(['success' => true]);
-                error_log("No changes made. Data is already up-to-date.");
-            }
-         }else{
-             echo json_encode(['success' => false]);
-             error_log("Query execution failed.");
-         }
+            // 실제로 데이터가 업데이트 되었음
+
+            // 알림 데이터 구성
+            require_once '../FCM/selectFcmTokenAndProfileImg.php';
+            $result = selectFcmTokenAndProfileImg($conn, $partnerId);
+            $token = $result['token'];
+            $userProfile = $result['profile_image'];
+            $name = $result['name'];
+            $messageData = [
+                'flag' => 'story_location_update_notification',
+                'title' => 'tiki taka',
+                'body' => $name.'님이 스토리 폴더를 수정했습니다. 확인해보세요!',
+                'userProfile' => $userProfile,
+                'folderId' => $folderId
+            ];
+
+    // FCM 서버에 알림 데이터를 보내기
+    // require_once : 다른 파일을 현재 스크립트에 포함시킬 때 사용
+    require_once '../FCM/sendFcmNotification.php';
+    $resultFCM = sendFcmNotification($token, $messageData);
+
+        if($resultFCM){
+            echo json_encode(["success" => true]);
+        }else{
+            error_log("게시 실패 ㅠ: sendFcmNotification() 실행시 문제가 생김");
+        }
+
+    }else{
+        // 데이터가 업데이트되지 않았음을 의미할 수 있음 (이미 같은 값이었을 경우 등)
+        echo json_encode(['success' => true]);
+        error_log("No changes made. Data is already up-to-date.");
+        }
+    }else{
+        echo json_encode(['success' => false]);
+        error_log("Query execution failed.");
+    }
 
     }catch (PDOException $e) {
         error_log("Database error: " . $e->getMessage());
     
     }
 }else {
-    error_log("Folder ID or Card ID is required.");
+    error_log("Folder ID or Card ID or PartnerId is required.");
 }
