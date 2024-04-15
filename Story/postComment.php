d5ba64e36ca8174caab3a804ea7fd6b24233e612
jieun <kindjjee@gmail.com>
2024-04-15 07:18:51 +0000

    댓글 편집 알림 데이터 수정

--- Story/postComment.php
+++ Story/postComment.php
@@ -13,9 +13,10 @@ error_log("Received JSON: " . $json);
 $data = json_decode($json);
 
 //PHP 코드에서 이 객체의 속성에 접근할 때는 JSON 데이터의 키 이름을 그대로 사용
-$cardId = $data -> card_id;
+$cardId = $data -> id;
 $userId = $data -> user_id;
 $commentText = $data -> comment_text;
+$partnerId = $data -> partnerId;
 
 // 댓글 INSERT 쿼리 준비 및 실행
 $sql = "INSERT INTO comment (card_id, user_id, comment_text) VALUES (:cardId, :userId, :commentText)";
@@ -23,7 +24,33 @@ $stmt = $conn -> prepare($sql);
 $success = $stmt -> execute([':cardId' => $cardId, ':userId' => $userId, ':commentText' => $commentText]);
 
 if($success){
-   echo json_encode(['success' => true]);
+
+    // 알림 데이터 구성
+    require_once '../FCM/selectFcmTokenAndProfileImg.php';
+    $result = selectFcmTokenAndProfileImg($conn, $partnerId, $userId);
+    $tokenRow = $result['fcmToken'];
+    $token = $tokenRow['token'];
+    $profileInfo = $result['profileInfo'];
+    $userImg = $profileInfo['profile_image'];
+    $name = $profileInfo['name'];
+    $messageData = [
+        'flag' => 'story_comment_notification',
+        'title' => 'tiki taka',
+        'body' => $name.'님이 댓글을 추가했습니다. 확인해보세요!',
+        'userProfile' => $userImg,
+        'cardId' => $cardId
+    ];
+
+    // FCM 서버에 알림 데이터를 보내기
+    // require_once : 다른 파일을 현재 스크립트에 포함시킬 때 사용
+    require_once '../FCM/sendFcmNotification.php';
+    $resultFCM = sendFcmNotification($token, $messageData);
+
+        if($resultFCM){
+            echo json_encode(["success" => true]);
+        }else{
+            error_log("게시 실패 ㅠ: sendFcmNotification() 실행시 문제가 생김");
+        }
 
 }else{
     echo json_encode(['success' => false]);
--- Story/updateComment.php
+++ Story/updateComment.php
@@ -10,20 +10,56 @@ $json = file_get_contents('php://input');
 $data = json_decode($json);
 
 //PHP 코드에서 이 객체의 속성에 접근할 때는 JSON 데이터의 키 이름을 그대로 사용
-$commentId = $data -> comment_id;
+$commentId = $data -> id;
+$userId = $data -> user_id;
 $commentText = $data -> comment_text;
+$partnerId = $data -> partnerId;
 
-$sql = "UPDATE comment SET comment_text = :commentText
-            WHERE comment_id = :commentId";
+$sql = "SELECT card_id from comment WHERE comment_id = :commentId";
         $stmt = $conn->prepare($sql);
-        $stmt->bindParam(':commentText', $commentText, PDO::PARAM_STR);
         $stmt->bindParam(':commentId', $commentId, PDO::PARAM_INT);
         $success = $stmt->execute();
-
         if($success){
-            if($stmt -> rowCount() > 0){
-                // 실제로 데이터가 업데이트 되었음
-                echo json_encode(['success' => true]);
+            $result = $stmt -> fetch(PDO::FETCH_ASSOC);
+            $cardId = $result['card_id'];
+        }
+
+$sql2 = "UPDATE comment SET comment_text = :commentText
+        WHERE comment_id = :commentId";
+        $stmt2 = $conn->prepare($sql2);
+        $stmt2->bindParam(':commentText', $commentText, PDO::PARAM_STR);
+        $stmt2->bindParam(':commentId', $commentId, PDO::PARAM_INT);
+        $success2 = $stmt2->execute();
+
+        if($success2){
+            if($stmt2 -> rowCount() > 0){
+            // 실제로 데이터가 업데이트 되었음
+            // 알림 데이터 구성
+            require_once '../FCM/selectFcmTokenAndProfileImg.php';
+            $result = selectFcmTokenAndProfileImg($conn, $partnerId, $userId);
+            $tokenRow = $result['fcmToken'];
+            $token = $tokenRow['token'];
+            $profileInfo = $result['profileInfo'];
+            $userImg = $profileInfo['profile_image'];
+            $name = $profileInfo['name'];
+            $messageData = [
+                'flag' => 'story_comment_update_notification',
+                'title' => 'tiki taka',
+                'body' => $name.'님이 댓글을 수정했습니다. 확인해보세요!',
+                'userProfile' => $userImg,
+                'cardId' => $cardId // 여기 추가하기
+            ];
+
+            // FCM 서버에 알림 데이터를 보내기
+            // require_once : 다른 파일을 현재 스크립트에 포함시킬 때 사용
+            require_once '../FCM/sendFcmNotification.php';
+            $resultFCM = sendFcmNotification($token, $messageData);
+
+                if($resultFCM){
+                    echo json_encode(["success" => true]);
+                }else{
+                    error_log("게시 실패 ㅠ: sendFcmNotification() 실행시 문제가 생김");
+                }
             }else{
                 echo json_encode(['success' => false]);
                 error_log("No data updated");
